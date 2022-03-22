51cd345e5b8a32d125450a357a99f0b8cebc39a9
bogdansimion <bogdansimion01@gmail.com>
2022-03-22 19:28:30 +0200

    added first test

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,8 +1,18 @@
 package service;
 
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
 
 import static org.junit.Assert.*;
 
@@ -18,5 +28,15 @@ public class ServiceTest {
 
     @Test
     public void saveStudent() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        assertEquals(service.saveStudent("01","nume1",1),1);
     }
 }
